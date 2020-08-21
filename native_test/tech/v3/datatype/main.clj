a7b1610db641e353af8e907f2048734081518e7b
Chris Nuernberger <chris@techascent.com>
2020-08-21 14:09:36 -0600

    Unified interface.

--- native_test/tech/v3/datatype/main.clj
+++ native_test/tech/v3/datatype/main.clj
@@ -3,16 +3,15 @@
               [tech.v3.datatype.protocols :as dtype-proto]
               [tech.v3.parallel.for :as parallel-for]
               [primitive-math :as pmath])
-  (:import [tech.v3.datatype DoubleReader])
+  (:import [tech.v3.datatype PrimitiveReader])
   (:gen-class))
 
 
 (defn -main
   [& args]
   (let [n-elems 10000000
-        ^DoubleReader darray (dtype-proto/->reader
-                              (array-buffer/array-buffer
-                               (double-array (range n-elems))) {})
+        ^PrimitiveReader darray (dtype-proto/->reader
+                                 (double-array (range n-elems)) {})
         sum (parallel-for/indexed-map-reduce
              n-elems
              (fn [^long start-idx ^long group-len]
@@ -20,7 +19,7 @@
                  (loop [idx start-idx
                         sum 0.0]
                    (if (< idx end-idx)
-                     (recur (unchecked-inc idx) (pmath/+ sum (.read darray idx)))
+                     (recur (unchecked-inc idx) (pmath/+ sum (.readDouble darray idx)))
                      sum))))
              (partial reduce +))]
     (println sum)
