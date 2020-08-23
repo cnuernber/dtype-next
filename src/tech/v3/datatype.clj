4d31870a39c44d3bd680bb447e3cf55ec6df9998
Chris Nuernberger <chris@techascent.com>
2020-08-23 11:15:19 -0600

    Added namespaces to require timing tests.

--- src/tech/v3/datatype.clj
+++ src/tech/v3/datatype.clj
@@ -1,3 +1,5 @@
 (ns tech.v3.datatype
   (:require [tech.v3.datatype.array-buffer]
-            [tech.v3.datatype.native-buffer]))
+            [tech.v3.datatype.native-buffer]
+            [tech.v3.datatype.base :as dtype-base]
+            [tech.v3.datatype.copy-make-container :as dtype-copy-make-container]))
