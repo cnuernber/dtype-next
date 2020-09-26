1a2d6a3d345e77737852cd7ec4f9eb9fa4a13ade
Chris Nuernberger <chris@techascent.com>
2020-09-26 13:35:20 -0600

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cnuernber/dtype-next "0.3.2"
+(defproject cnuernber/dtype-next "0.3.3-SNAPSHOT"
   :description "A Clojure library designed to aid in the implementation of
 high performance algorithms and systems"
   :url "http://github.com/cnuernber/dtype-next"
