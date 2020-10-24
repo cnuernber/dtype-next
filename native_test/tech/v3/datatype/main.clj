3b76e1ca3bcf876b881b9560b88b9a62bb6abe66
Chris Nuernberger <chris@techascent.com>
2020-10-24 14:52:46 -0600

    Finally have main.clj compiling to correct size and running without warnings.

--- native_test/tech/v3/datatype/main.clj
+++ native_test/tech/v3/datatype/main.clj
@@ -1,13 +1,14 @@
 (ns tech.v3.datatype.main
-  (:require [tech.v3.datatype.array-buffer :as ary-buf]
-            [tech.v3.datatype.native-buffer :as native-buffer]
-            [tech.v3.datatype.copy-make-container :as copy-cmc])
+  (:require [tech.v3.datatype :as dtype]
+            [tech.v3.datatype.datetime :as dtype-dt]
+            [tech.v3.tensor :as dtt])
   (:gen-class))
 
 (set! *warn-on-reflection* true)
 
 (defn -main
   [& args]
-  (let [data (copy-cmc/make-container :jvm-heap :float32 (range 10))]
-    (println data))
+  (let [data (dtt/->tensor (partition 3 (range 9)) :datatype :float32)
+        ld (dtype-dt/local-date)]
+    (println data ld))
   0)
