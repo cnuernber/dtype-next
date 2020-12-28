2be44d4b3a06049ea7783bef38354253a4460c47
Carsten Behring <carsten.behring@gmail.com>
2020-12-28 22:59:31 +0100

    implementation of MmapStringList (#8)
    * first working code
    
    * cleaned up simple implementation
    
    * renamed test file
    
    * pass mmap and output-stream as fields
    
    * replace dirt-flag cy checking mmap for nil
    
    * added benchmark time
    
    * removed becnhmarking from test file
    
    * fixed reading atom issue
    
    * using FileOutputStream + sync()
    
    * changed to FileChannels
    
    * generalized using 2 conversion functions
    
    * go back to FileChannel

---
+++ test/tech/v3/datatype/mmap_list_test.clj
@@ -0,0 +1,80 @@
+(ns tech.v3.datatype.mmap-string-list-test
+  (:require [clojure.test :refer [deftest is]]
+            [tech.v3.datatype.mmap-list :as mmap-list])
+  (:import java.net.URI
+           java.nio.channels.FileChannel
+           [java.nio.file Paths StandardOpenOption]))
+
+(deftest test-add-read
+  (let [mmap-file (java.io.File/createTempFile "strings" ".mmap")
+        positions (atom [])
+        string-list (mmap-list/->MmapList
+                     #(String. %)
+                     #(.getBytes %)
+                     :string
+                     (.getPath mmap-file)
+                     (FileChannel/open  (.toPath mmap-file)
+                                        (into-array [StandardOpenOption/APPEND]))
+
+                     positions
+                     (atom nil)
+                     )]
+
+
+    (.addObject string-list "test")
+    (is (= 1 (.lsize string-list)))
+    (is (= "test" (.readObject string-list 0)))
+    ))
+
+(deftest test-add-read-various
+  (let [mmap-file (java.io.File/createTempFile "strings" ".mmap")
+        positions (atom [])
+        string-list (mmap-list/->MmapList
+                     #(String. %)
+                     #(.getBytes %)
+                     :string
+                     (.getPath mmap-file)
+                     (FileChannel/open  (.toPath mmap-file)
+                                        (into-array [StandardOpenOption/APPEND]))
+                     positions
+                     (atom nil)
+                     )
+
+
+        _ (.addObject string-list "hello")
+        _ (.addObject string-list "my world")]
+    (is (= 2 (.lsize string-list)))
+    (is (= "hello" (.readObject string-list 0)))
+    (is (= "my world" (.readObject string-list 1)))
+    ))
+
+
+
+
+
+
+(comment
+  (spit "/tmp/test.mmap" "")
+  (def positions (atom []))
+  (def my-list (mmap-list/->MmapList
+                #(String. %)
+                #(.getBytes %)
+                :string
+                "/tmp/test.mmap"
+                (FileChannel/open  (Paths/get (URI. "file:/tmp/test.mmap"))
+                                   (into-array [StandardOpenOption/APPEND]))
+                positions
+                (atom nil)
+                ))
+  (.addObject my-list "super world")
+  (.lsize my-list)
+  (.readObject my-list 0)
+  (.writeObject my-list 0 "")
+
+  (.mmap my-list)
+
+  @positions
+
+  (.nth my-list 0)
+
+  )
