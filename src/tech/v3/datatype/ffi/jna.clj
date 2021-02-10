f041be33a32eff36437eb2855208a6d2382484b8
Chris Nuernberger <chris@techascent.com>
2021-02-10 16:59:09 -0700

    Reworking codegen a bit.

--- src/tech/v3/datatype/ffi/jna.clj
+++ src/tech/v3/datatype/ffi/jna.clj
@@ -36,7 +36,7 @@
       (.getGlobalVariableAddress (str symbol-name))))
 
 (defn dtype->insn
-  [argtype]
+  [ptr-disposition argtype]
   (case argtype
     :int8 :byte
     :int16 :short
@@ -44,9 +44,17 @@
     :int64 :long
     :float32 :float
     :float64 :double
-    :pointer (dtype->insn (ffi/size-t-type))
-    :pointer? (dtype->insn (ffi/size-t-type))
-    :size-t (dtype->insn (ffi/size-t-type))
+    :pointer (case ptr-disposition
+               :ptr-as-int (dtype->insn :ptr-as-int (ffi/size-t-type))
+               :ptr-as-obj Object
+               :ptr-as-ptr tech.v3.datatype.ffi.Pointer
+               :ptr-as-platform Pointer)
+    :pointer? (case ptr-disposition
+               :ptr-as-int (dtype->insn :ptr-as-int (ffi/size-t-type))
+               :ptr-as-obj Object
+               :ptr-as-ptr tech.v3.datatype.ffi.Pointer
+               :ptr-as-platform Pointer)
+    :size-t (dtype->insn :ptr-as-int (ffi/size-t-type))
     :void :void
     (do
       (errors/when-not-errorf
@@ -57,209 +65,42 @@
 
 
 (defn argtypes->insn-desc
-  [argtypes rettype]
-  (mapv dtype->insn (concat argtypes [rettype])))
+  ([argtypes rettype ptr-disposition]
+   (mapv (partial dtype->insn ptr-disposition) (concat argtypes [rettype])))
+  ([argtypes rettype]
+   (argtypes->insn-desc argtypes rettype :ptr-as-int)))
 
 
-(defn argtypes->obj-insn-desc
-  [argtypes rettype]
-  (vec (concat (map (fn [argtype]
-                      (case argtype
-                        :int8 :byte
-                        :int16 :short
-                        :int32 :int
-                        :int64 :long
-                        :float32 :float
-                        :float64 :double
-                        :pointer Object
-                        :pointer? Object
-                        :size-t (case (ffi/size-t-type)
-                                  :int32 :int
-                                  :int64 :long)))
-                    argtypes)
-               [(case rettype
-                  :void :void
-                  :int8 :byte
-                  :int16 :short
-                  :int32 :int
-                  :int64 :long
-                  :float32 :float
-                  :float64 :double
-                  :pointer tech.v3.datatype.ffi.Pointer
-                  :pointer? tech.v3.datatype.ffi.Pointer
-                  :size-t (ffi/size-t-type))])))
-
-(def ptr-cast
+(defn make-ptr-cast
+  [mem-fn-name]
   (concat [[:aload 0]
-           [:getfield :this "asPointer" IFn]
+           [:getfield :this mem-fn-name IFn]
            ;;Swap the IFn 'this' ptr and the argument on the stack
            [:swap]
            [:invokeinterface IFn "invoke" [Object Object]]
-           [:checkcast Long]
-           [:invokevirtual Long "longValue" [:long]]]
-          (when (= :int32 (ffi/size-t-type))
-            [[:l2i]])))
+           [:checkcast Pointer]]))
 
 
-(def ptr?-cast
-  (concat [[:aload 0]
-           [:getfield :this "asPointerQ" IFn]
-           ;;Swap the IFn 'this' ptr and the argument on the stack
-           [:swap]
-           [:invokeinterface IFn "invoke" [Object Object]]
-           [:checkcast Long]
-           [:invokevirtual Long "longValue" [:long]]]
-          (when (= :int32 (ffi/size-t-type))
-            [[:l2i]])))
-
-
-(defn argtype-cast
-  [argtype]
-  (case argtype
-    :int8 [[:invokestatic NumericConversions "numberCast" [Object Number]]
-           [:invokestatic RT "uncheckedByteCast" [Object Byte/TYPE]]]
-    :int16 [[:invokestatic NumericConversions "numberCast" [Object Number]]
-            [:invokestatic RT "uncheckedShortCast" [Object Short/TYPE]]]
-    :int32 [[:invokestatic NumericConversions "numberCast" [Object Number]]
-            [:invokestatic RT "uncheckedIntCast" [Object Integer/TYPE]]]
-    :int64 [[:invokestatic NumericConversions "numberCast" [Object Number]]
-            [:invokestatic RT "uncheckedLongCast" [Object Long/TYPE]]]
-    :float32 [[:invokestatic NumericConversions "numberCast" [Object Number]]
-              [:invokestatic RT "uncheckedFloatCast" [Object Float/TYPE]]]
-    :float64 [[:invokestatic NumericConversions "numberCast" [Object Number]]
-              [:invokestatic RT "uncheckedDoubleCast" [Object Double/TYPE]]]
-    :size-t (argtype-cast (ffi/size-t-type))
-    :pointer ptr-cast
-    :pointer? ptr-cast))
-
-
+(def ptr-cast (make-ptr-cast "asPointer"))
+(def ptr?-cast (make-ptr-cast "asPointerQ"))
 (def ptr-return
-  (concat
-   (when (= :int32 (ffi/size-t-type))
-     [[:i2l]])
-   [[:lstore 1]
-    [:new tech.v3.datatype.ffi.Pointer]
-    [:dup]
-    [:lload 1]
-    [:invokespecial tech.v3.datatype.ffi.Pointer :init [:long :void]]
-    [:areturn]]))
+  [[:invokestatic Pointer "nativeValue" [Pointer :long]]
+   [:lstore 1]
+   [:new tech.v3.datatype.ffi.Pointer]
+   [:dup]
+   [:lload 1]
+   [:invokespecial tech.v3.datatype.ffi.Pointer :init [:long :void]]
+   [:areturn]])
 
 
-(defn emit-invoke
-  [cls-name symbol-name argtypes rettype]
-  (concat
-   (->> argtypes
-        (map-indexed (fn [idx argtype]
-                       (let [arg-idx (inc (long idx))]
-                         (concat [[:aload arg-idx]]
-                                 (argtype-cast argtype)))))
-        (apply concat))
-   [[:invokestatic (str "tech.v3.datatype.ffi.jna." cls-name)
-     symbol-name (argtypes->insn-desc argtypes rettype)]]
-   (case (if (= rettype :size-t)
-           (ffi/size-t-type)
-           rettype)
-     :int8 [[:invokestatic Byte "valueOf" [:byte Byte]]
-            [:areturn]]
-     :int16 [[:invokestatic Short "valueOf" [:short Short]]
-             [:areturn]]
-     :int32 [[:invokestatic Integer "valueOf" [:int Integer]]
-             [:areturn]]
-     :int64 [[:invokestatic Long "valueOf" [:long Long]]
-             [:areturn]]
-     :float32 [[:invokestatic Float "valueOf" [:float Float]]
-               [:areturn]]
-     :float64 [[:invokestatic Double "valueOf" [:double Double]]
-               [:areturn]]
-     :pointer ptr-return
-     :pointer? ptr-return
-     (if (or (nil? rettype)
-             (= :void rettype))
-       [[:return]]
-       [[:areturn]]))))
+(defn ptr-value
+  ^Pointer [item]
+  (Pointer. (ffi/ptr-value item)))
 
 
-(defn generate-class
-  [symbol-name rettype argtypes]
-  (let [cls-name (str "Invoker_" (name (gensym)))
-        full-cls-name (symbol (str "tech.v3.datatype.ffi.jna." cls-name))]
-    {:name full-cls-name
-     :interfaces [IFn IObj]
-     :fields [{:flags #{:public :final}
-               :name "asPointer"
-               :type IFn}
-              {:flags #{:public :final}
-               :name "asPointerQ"
-               :type IFn}
-              {:flags #{:public :final}
-               :name "metadata"
-               :type IPersistentMap}]
-     :methods [{:flags #{:public}
-                :name :init
-                :desc [IFn IFn IPersistentMap :void]
-                :emit [[:aload 0]
-                       [:invokespecial :super :init [:void]]
-                       [:aload 0]
-                       [:aload 1]
-                       [:checkcast IFn]
-                       [:putfield :this "asPointer" IFn]
-                       [:aload 0]
-                       [:aload 2]
-                       [:checkcast IFn]
-                       [:putfield :this "asPointerQ" IFn]
-                       [:aload 0]
-                       [:aload 3]
-                       [:checkcast IFn]
-                       [:putfield :this "metadata" IPersistentMap]
-                       [:return]]}
-               {:flags #{:public :static :native}
-                :name symbol-name
-                :desc (argtypes->insn-desc argtypes rettype)}
-               {:flags #{:public}
-                :name :invoke
-                :desc (vec (repeat (inc (count argtypes)) Object))
-                :emit (emit-invoke cls-name symbol-name argtypes rettype)}
-               {:flags #{:public}
-                :name :meta
-                :desc [IPersistentMap]
-                :emit [[:aload 0]
-                       [:getfield :this "metadata" IPersistentMap]
-                       [:areturn]]}
-               {:flags #{:public}
-                :name :withMeta
-                :desc [IPersistentMap Object]
-                :emit [[:new full-cls-name]
-                       [:dup]
-                       [:aload 0]
-                       [:getfield :this "asPointer" IFn]
-                       [:aload 0]
-                       [:getfield :this "asPointerQ" IFn]
-                       [:aload 1]
-                       [:invokespecial full-cls-name :init [IFn IFn IPersistentMap :void]]
-                       [:areturn]]}
-               ]}))
-
-
-(defn cls->inst
-  [invoker-cls]
-  (let [^Constructor ctor (first (.getDeclaredConstructors ^Class invoker-cls))]
-    (.newInstance ctor (object-array [ffi/ptr-value ffi/ptr-value? {}]))))
-
-
-(defn generate-binding
-  [symbol-name rettype argtypes options]
-  (let [cls-def (generate-class symbol-name rettype argtypes)
-        cls (insn/define cls-def)
-        inst (cls->inst cls)]
-    [cls inst]))
-
-
-(defn find-function
-  [libname symbol-name rettype argtypes options]
-  (let [library (load-library libname)
-        [cls inst] (generate-binding symbol-name rettype argtypes options)]
-    (com.sun.jna.Native/register ^Class cls library)
-    inst))
+(defn ptr-value?
+  ^Pointer [item]
+  (Pointer. (ffi/ptr-value? item)))
 
 
 (defn emit-library-constructor
@@ -271,12 +112,12 @@
    [:invokestatic NativeLibrary "getInstance" [String NativeLibrary]]
    [:invokestatic com.sun.jna.Native "register" [Class NativeLibrary :void]]
    [:aload 0]
-   [:ldc "tech.v3.datatype.ffi"]
+   [:ldc "tech.v3.datatype.ffi.jna"]
    [:ldc "ptr-value"]
    [:invokestatic ClojureHelper "findFn" [String String IFn]]
    [:putfield :this "asPointer" IFn]
    [:aload 0]
-   [:ldc "tech.v3.datatype.ffi"]
+   [:ldc "tech.v3.datatype.ffi.jna"]
    [:ldc "ptr-value?"]
    [:invokestatic ClojureHelper "findFn" [String String IFn]]
    [:putfield :this "asPointerQ" IFn]
@@ -302,7 +143,9 @@
   [inner-cls [fn-name fn-def]]
   {:name fn-name
    :flags #{:public}
-   :desc (argtypes->obj-insn-desc (:argtypes fn-def) (:rettype fn-def))
+   :desc (->> (concat (map (partial dtype->insn :ptr-as-obj) (:argtypes fn-def))
+                      [(dtype->insn :ptr-as-ptr (:rettype fn-def))])
+              (vec))
    :emit
    (vec (concat
          (->> (args->indexes-args (:argtypes fn-def))
@@ -320,7 +163,7 @@
                    :float32 [[:fload arg-idx]]
                    :float64 [[:dload arg-idx]]))))
          [[:invokestatic inner-cls (name fn-name)
-           (argtypes->insn-desc (:argtypes fn-def) (:rettype fn-def))]
+           (argtypes->insn-desc (:argtypes fn-def) (:rettype fn-def) :ptr-as-platform)]
           (case (ffi/lower-type (:rettype fn-def))
             :void [:return]
             :int8 [:ireturn]
@@ -333,7 +176,9 @@
 
 
 (defn define-library
-  [library-symbol fn-defs options]
+  [library-symbol fn-defs {:keys [emit-cls?]
+                           :or {emit-cls? true}
+                           :as options}]
   ;; First we define the inner class which contains the typesafe static methods
   (let [inner-name (symbol (str library-symbol "$inner"))]
     (-> {:name inner-name
@@ -343,31 +188,68 @@
                            :name k
                            :desc (argtypes->insn-desc
                                   (:argtypes v)
-                                  (:rettype v))})
+                                  (:rettype v)
+                                  :ptr-as-platform)})
                         fn-defs)}
         (insn/visit)
         (insn/write))
-    (-> {:name library-symbol
-         :flags #{:public}
-         :fields [{:name "asPointer"
-                   :type IFn
-                   :flags #{:public :final}}
-                  {:name "asPointerQ"
-                   :type IFn
-                   :flags #{:public :final}}]
-         :methods (vec (concat
-                        [{:name :init
-                          :desc [String :void]
-                          :emit (emit-library-constructor inner-name)}]
-                        (mapv (partial emit-wrapped-fn inner-name) fn-defs)))}
-        (insn/visit)
-        (insn/write))
-    {:inner-cls inner-name
-     :library library-symbol}))
+    (let [cls-data
+          {:name library-symbol
+           :flags #{:public}
+           :fields [{:name "asPointer"
+                     :type IFn
+                     :flags #{:public :final}}
+                    {:name "asPointerQ"
+                     :type IFn
+                     :flags #{:public :final}}]
+           :methods (vec (concat
+                          [{:name :init
+                            :desc [String :void]
+                            :emit (emit-library-constructor inner-name)}]
+                          (mapv (partial emit-wrapped-fn inner-name) fn-defs)))}]
+      (when emit-cls?
+        (-> cls-data
+            (insn/visit)
+            (insn/write)))
+      {:inner-cls inner-name
+       :library library-symbol
+       :library-class-def cls-data})))
+
+
+(defn find-function
+  [libname symbol-name rettype argtypes options]
+  (let [lib-symbol (:library (define-library
+                               (symbol (str "tech.v3.datatype.ffi.jna.Invoker_"
+                                            (name (gensym))))
+                               {symbol-name {:rettype rettype
+                                             :argtypes argtypes}}
+                               {}))
+        eval-data
+        (list 'do (list 'import lib-symbol)
+              (list 'let ['libinst (list 'new lib-symbol libname)]
+                    (list 'fn (vec (map (fn[idx]
+                                          (symbol (str "arg_" idx)))
+                                        (range (count argtypes))))
+                          (concat
+                           [(symbol (str "." (name symbol-name))) 'libinst]
+                           (->> argtypes
+                                (map-indexed
+                                 (fn [idx argtype]
+                                   (let [arg-sym (symbol (str "arg_" idx))]
+                                     (case (ffi/lower-type argtype)
+                                       :int8 (list 'unchecked-byte arg-sym)
+                                       :int16 (list 'unchecked-short arg-sym)
+                                       :int32 (list 'unchecked-int arg-sym)
+                                       :int64 (list 'unchecked-long arg-sym)
+                                       :float32 (list 'unchecked-float arg-sym)
+                                       :float64 (list 'unchecked-double arg-sym)
+                                       :pointer arg-sym
+                                       :pointer? arg-sym)))))))))]
+    (eval eval-data)))
 
 
 (def ffi-fns {:library-instance? library-instance?
               :load-library load-library
               :find-symbol find-symbol
-              :find-function find-function
-              :define-library define-library})
+              :define-library define-library
+              :find-function find-function})
