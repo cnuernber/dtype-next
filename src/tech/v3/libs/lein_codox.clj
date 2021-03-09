1220a71c8fcf1e84d75d8e8ea9002b58c4bee183
Chris Nuernberger <chris@techascent.com>
2021-03-09 09:32:55 -0700

    Updated to support deps-based codox pathway

--- src/tech/v3/libs/lein_codox.clj
+++ src/tech/v3/libs/lein_codox.clj
@@ -24,19 +24,29 @@
 
 
 (defn get-codox-options
-  []
-  (if (.exists (io/file "project.clj"))
-    (let [proj-file (edn/read-string (slurp "project.clj"))
-          proj-name (name (second proj-file))
-          val-map (find-vals proj-file [:description :profiles])
-          codox-section (get-in val-map [:profiles :codox :codox])]
-      (merge
-       {:name proj-name
-        :description (:description val-map "")
-        :version (nth proj-file 2)}
-       codox-section))))
+  [args]
+  (let [base-argmap (first args)]
+    (merge base-argmap
+           (cond
+             (.exists (io/file "deps.edn"))
+             (let [deps-edn (edn/read-string (slurp "deps.edn"))
+                   retval (->> (:arg-paths base-argmap)
+                               (map #(get-in deps-edn %))
+                               (apply merge))]
+               (println "RETVAL is" retval)
+               retval)
+             (.exists (io/file "project.clj"))
+             (let [proj-file (edn/read-string (slurp "project.clj"))
+                   proj-name (name (second proj-file))
+                   val-map (find-vals proj-file [:description :profiles])
+                   codox-section (get-in val-map [:profiles :codox :codox])]
+               (merge
+                {:name proj-name
+                 :description (:description val-map "")
+                 :version (nth proj-file 2)}
+                codox-section))))))
 
 
 (defn -main
   [& args]
-  (codox/generate-docs (get-codox-options)))
+  (codox/generate-docs (get-codox-options args)))
