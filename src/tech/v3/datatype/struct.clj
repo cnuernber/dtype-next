eb718a87ac7d698f764e1df3b43d9e27fe66a272
Chris Nuernberger <chris@techascent.com>
2021-03-24 12:57:18 -0600

    Updating struct documentation.

--- src/tech/v3/datatype/struct.clj
+++ src/tech/v3/datatype/struct.clj
@@ -1,9 +1,43 @@
 (ns tech.v3.datatype.struct
   "Structs are datatypes composed of primitive datatypes or other structs.
   Similar to records except they do not support string or object columns,
-  only numeric values.  They have memset-0 initialization, memcpy copy,
-  and defined equals and hash parameters all based on the actual binary
-  representation of the data in the struct."
+  only numeric values.  They have memset-0 initialization, memcpy copy
+  semantics.  For correct equals, hashing, convert struct into a normal
+  persistent map via `into`.
+
+  Example:
+
+```clojure
+user> (require '[tech.v3.datatype :as dtype])
+nil
+user> (require '[tech.v3.datatype.struct :as dt-struct])
+nil
+user> (define-datatype! :vec3 [{:name :x :datatype :float32}
+                         {:name :y :datatype :float32}
+                         {:name :z :datatype :float32}])
+Syntax error compiling at (*cider-repl cnuernber/dtype-next:localhost:36129(clj)*:47:7).
+Unable to resolve symbol: define-datatype! in this context
+user> (dt-struct/define-datatype! :vec3 [{:name :x :datatype :float32}
+                                         {:name :y :datatype :float32}
+                                         {:name :z :datatype :float32}])
+{:datatype-size 12,
+ :datatype-width 4,
+ :data-layout
+ [{:name :x, :datatype :float32, :offset 0, :n-elems 1}
+  {:name :y, :datatype :float32, :offset 4, :n-elems 1}
+  {:name :z, :datatype :float32, :offset 8, :n-elems 1}],
+ :layout-map
+ {:x {:name :x, :datatype :float32, :offset 0, :n-elems 1},
+  :y {:name :y, :datatype :float32, :offset 4, :n-elems 1},
+  :z {:name :z, :datatype :float32, :offset 8, :n-elems 1}},
+ :datatype-name :vec3}
+user> (dt-struct/new-struct :vec3)
+{:x 0.0, :y 0.0, :z 0.0}
+user> (.put *1 :x 2.0)
+nil
+user> *2
+{:x 2.0, :y 0.0, :z 0.0}
+```"
   (:require [tech.v3.datatype.casting :as casting]
             [tech.v3.datatype.base :as dtype-base]
             [tech.v3.datatype.protocols :as dtype-proto]
@@ -20,10 +54,11 @@
 (set! *unchecked-math* :warn-on-boxed)
 
 
-(defonce struct-datatypes (ConcurrentHashMap.))
+(defonce ^:private struct-datatypes (ConcurrentHashMap.))
 
 
 (defn datatype-size
+  "Return the size, in bytes, of a datatype."
   ^long [datatype]
   (if-let [struct-dtype (.get ^ConcurrentHashMap struct-datatypes datatype)]
     (long (:datatype-size struct-dtype))
@@ -32,6 +67,8 @@
 
 
 (defn datatype-width
+  "Return the width or the of a datatype.  The width dictates what address
+  the datatype can start at when embedded in another datatype."
   ^long [datatype]
   (if-let [struct-dtype (.get ^ConcurrentHashMap struct-datatypes datatype)]
     (long (:datatype-width struct-dtype))
@@ -39,7 +76,7 @@
         (casting/numeric-byte-width))))
 
 
-(defn widen-offset
+(defn- widen-offset
   "Ensure the offset starts at the appropriate boundary for the datatype width."
   ^long [^long offset ^long datatype-width]
   (let [rem-result (rem offset datatype-width)]
@@ -49,11 +86,12 @@
 
 
 (defn struct-datatype?
+  "Returns true of this datatype denotes a struct datatype."
   [datatype]
   (.containsKey ^ConcurrentHashMap struct-datatypes datatype))
 
 
-(defn layout-datatypes
+(defn- layout-datatypes
   [datatype-seq]
   (let [[datatype-seq widest-datatype current-offset]
         (->> datatype-seq
@@ -89,13 +127,14 @@
 
 
 (defn get-struct-def
+  "Get a previously constructed struct definition."
   [datatype]
   (if-let [retval (.get ^ConcurrentHashMap struct-datatypes datatype)]
     retval
     (throw (Exception. (format "Datatype %s is not a struct definition." datatype)))))
 
 
-(defn get-datatype
+(defn- get-datatype
   [datatype-name]
   (.getOrDefault ^ConcurrentHashMap struct-datatypes
                  datatype-name datatype-name))
@@ -103,48 +142,70 @@
 
 (defn offset-of
   "Returns a tuple of [offset dtype]."
-  [{:keys [layout-map] :as struct-def} property-vec]
-  (if-not (instance? RandomAccess property-vec)
-    (if-let [retval (get layout-map property-vec)]
-      [(:offset retval) (:datatype retval)]
-      (throw (Exception. (format "Property not found: %s" property-vec))))
-    (let [^List property-vec (if-not (instance? RandomAccess property-vec)
-                               [property-vec]
-                               property-vec)
-          n-lookup (count property-vec)]
-      (loop [idx 0
-             n-prop-elems 0
-             prop-datatype nil
-             struct-def struct-def
-             offset 0]
-        (if (< idx n-lookup)
-          (let [next-val (.get property-vec idx)
-                [offset
-                 struct-def
-                 n-prop-elems
-                 prop-datatype]
-                (if (number? next-val)
-                  (let [next-val (long next-val)]
-                    (when-not (< next-val n-prop-elems)
-                      (throw (Exception. "Indexed property access out of range")))
-                    [(+ offset (* next-val (long (datatype-size prop-datatype))))
-                     (get-datatype prop-datatype)
-                     0
-                     prop-datatype])
-                  (if-let [data-val (get-in struct-def [:layout-map next-val])]
-                    [(+ offset (long (:offset data-val)))
-                     (get-datatype (:datatype data-val))
-                     (long (:n-elems data-val))
-                     (:datatype data-val)]
-                    (throw (Exception.
-                            (format "Could not find property %s in %s"
-                                    next-val (:datatype-name struct-def))))))]
-            (recur (inc idx) (long n-prop-elems) prop-datatype
-                   struct-def (long offset)))
-          [offset prop-datatype])))))
+  [struct-dtype-or-struct-def property-vec]
+  (let [{:keys [layout-map] :as struct-def}
+        (if (map? struct-dtype-or-struct-def)
+          struct-dtype-or-struct-def
+          (get-struct-def struct-dtype-or-struct-def))]
+    (if-not (instance? RandomAccess property-vec)
+      (if-let [retval (get layout-map property-vec)]
+        [(:offset retval) (:datatype retval)]
+        (throw (Exception. (format "Property not found: %s" property-vec))))
+      (let [^List property-vec (if-not (instance? RandomAccess property-vec)
+                                 [property-vec]
+                                 property-vec)
+            n-lookup (count property-vec)]
+        (loop [idx 0
+               n-prop-elems 0
+               prop-datatype nil
+               struct-def struct-def
+               offset 0]
+          (if (< idx n-lookup)
+            (let [next-val (.get property-vec idx)
+                  [offset
+                   struct-def
+                   n-prop-elems
+                   prop-datatype]
+                  (if (number? next-val)
+                    (let [next-val (long next-val)]
+                      (when-not (< next-val n-prop-elems)
+                        (throw (Exception. "Indexed property access out of range")))
+                      [(+ offset (* next-val (long (datatype-size prop-datatype))))
+                       (get-datatype prop-datatype)
+                       0
+                       prop-datatype])
+                    (if-let [data-val (get-in struct-def [:layout-map next-val])]
+                      [(+ offset (long (:offset data-val)))
+                       (get-datatype (:datatype data-val))
+                       (long (:n-elems data-val))
+                       (:datatype data-val)]
+                      (throw (Exception.
+                              (format "Could not find property %s in %s"
+                                      next-val (:datatype-name struct-def))))))]
+              (recur (inc idx) (long n-prop-elems) prop-datatype
+                     struct-def (long offset)))
+            [offset prop-datatype]))))))
 
 
 (defn define-datatype!
+  "Define a new struct datatype.
+
+  * `datatype-name` - keyword datatype name.
+  * `datatype-seq` - Sequence of maps with the keys `{:name :datatype}`
+     which describe the new datatype.
+
+  Returns the new struct defintion.
+
+  Example:
+
+```clojure
+(define-datatype! :vec3 [{:name :x :datatype :float32}
+                         {:name :y :datatype :float32}
+                         {:name :z :datatype :float32}])
+
+(define-datatype! :segment [{:name :begin :datatype :vec3}
+                            {:name :end :datatype :vec3}])
+```"
   [datatype-name datatype-seq]
   (let [new-datatype (-> (layout-datatypes datatype-seq)
                          (assoc :datatype-name datatype-name))]
@@ -155,7 +216,7 @@
 (declare inplace-new-struct)
 
 
-(defmacro ensure-binary-buffer!
+(defmacro ^:private ensure-binary-buffer!
   []
   `(do
      (when-not ~'cached-buffer
@@ -163,11 +224,17 @@
      ~'cached-buffer))
 
 
-(deftype Struct [struct-def
-                 buffer
-                 ^{:unsynchronized-mutable true
-                   :tag BinaryBuffer} cached-buffer
-                 metadata]
+(deftype ^{:doc "Struct instance.  Derives most-notably from `java.util.Map` and
+ `clojure.lang.ILookup`.  Metadata (meta, with-meta, vary-meta) is supported.
+  Imporant member variables are:
+
+ * `.struct-def - The struct definition of this instance.
+ * `.buffer - The underlying backing store of this instance."}
+    Struct [struct-def
+            buffer
+            ^{:unsynchronized-mutable true
+              :tag BinaryBuffer} cached-buffer
+            metadata]
   dtype-proto/PDatatype
   (datatype [m] (:datatype-name struct-def))
   dtype-proto/PECount
@@ -270,6 +337,10 @@
 
 
 (defn inplace-new-struct
+  "Create a new struct in-place in the backing store.  The backing store must
+  either be convertible to a native buffer or a byte-array.
+
+  Returns a new Struct datatype."
   (^Struct [datatype backing-store options]
    (let [struct-def (get-struct-def datatype)]
      (Struct. struct-def backing-store nil {})))
@@ -278,6 +349,17 @@
 
 
 (defn new-struct
+  "Create a new struct.  By default this will use a byte array (:jvm-heap memory).
+  Returns a new struct.
+
+  Options are passed into dtype/make-container so container-specific options apply.
+
+  Options:
+
+  * `:container-type` - Defaults to `:jvm-heap` but often you want `:native-heap`
+  * `:resource-type` - If `:native-heap` `:container-type` is chosen, this dictates
+    the resource strategy.  Options are the same as
+    `tech.v3.datatype.native-buffer/malloc`."
   (^Struct [datatype options]
    (let [struct-def (get-struct-def datatype)
          ;;binary read/write to nio buffers is faster than our writer-wrapper
@@ -308,6 +390,8 @@
       (dtype-cmc/copy! (.buffer value) dst-buf))))
 
 
+;;Work in progress.  This creates a set of columns, not unlike a dataset from an
+;;array of structs.
 (deftype ArrayOfStructs [struct-def
                          ^long elem-size
                          ^long n-elems
@@ -360,7 +444,7 @@
                                                              elem-size))))
 
 
-(defn inplace-new-array-of-structs
+(defn ^:no-doc inplace-new-array-of-structs
   ([datatype buffer options]
    (let [struct-def (get-struct-def datatype)
          elem-size (long (:datatype-size struct-def))
@@ -377,7 +461,7 @@
    (inplace-new-array-of-structs datatype buffer {})))
 
 
-(defn new-array-of-structs
+(defn ^:no-doc new-array-of-structs
   ([datatype n-elems options]
    (let [struct-def (get-struct-def datatype)
          n-elems (long n-elems)
