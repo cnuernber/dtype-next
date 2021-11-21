71a60679aeccbd7104e2e2b69283f73e64ad982a
Chris Nuernberger <chris@techascent.com>
2021-11-21 09:17:43 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cnuernber/dtype-next "8.042"
+(defproject cnuernber/dtype-next "8.043-SNAPSHOT"
   :description "A Clojure library designed to aid in the implementation of high performance algorithms and systems."
   :url "http://github.com/cnuernber/dtype-next"
   :license {:name "EPL-2.0"
