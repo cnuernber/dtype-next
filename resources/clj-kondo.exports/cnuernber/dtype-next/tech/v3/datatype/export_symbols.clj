5f8d29ddbbaf13502b80211a913dfaabe67fcde0
Chris Nuernberger <chris@techascent.com>
2021-11-29 15:03:17 -0700

    One large step further.

--- resources/clj-kondo.exports/cnuernber/dtype-next/tech/v3/datatype/export_symbols.clj
+++ resources/clj-kondo.exports/cnuernber/dtype-next/tech/v3/datatype/export_symbols.clj
@@ -1,31 +1,24 @@
-(ns tech.v3.datatype.export-symbols)
+(ns tech.v3.datatype.export-symbols
+  (:require [clj-kondo.hooks-api :as hooks-api]))
 
 (defmacro export-symbols
   [src-ns & symbol-list]
-  `(do
-     ~@(->> symbol-list
-            (mapv
-             (fn [sym-name]
-               `(let [varval# (requiring-resolve (symbol ~(name src-ns)
-                                                         ~(name sym-name)))
-                      var-meta# (meta varval#)]
-                  (when-not varval#
-                    (throw (ex-info
-                            (format "Failed to find symbol '%s' in namespace '%s'"
-                                    ~(name sym-name) ~(name src-ns))
-                            {:symbol '~sym-name
-                             :src-ns '~src-ns})))
-                  (when (:macro var-meta#)
-                    (throw
-                     (ex-info
-                      (format "Cannot export macros as this breaks aot: %s"
-                              '~sym-name)
-                      {:symbol '~sym-name})))
-                  (def ~(symbol (name sym-name)) @varval#)
-                  (alter-meta! #'~(symbol (name sym-name))
-                               merge
-                               (select-keys var-meta#
-                                            [:file :line :column
-                                             :doc
-                                             :column :tag
-                                             :arglists]))))))))
+  (let [analysis (:clj (hooks-api/ns-analysis src-ns))]
+    `(do
+       ~@(->> symbol-list
+              (mapv
+               (fn [sym-name]
+                 (when-let [fn-data (get analysis sym-name)]
+                   (if-let [arities (get fn-data :fixed-arities)]
+                     `(defn ~sym-name
+                        ~@(->> arities
+                               (map (fn [arity]
+                                      (let [argvec (mapv
+                                                    #(symbol (str "arg-" %))
+                                                    (range arity))]
+                                        `(~argvec (apply + ~argvec)
+                                          ;;This line is to disable the type detection of clj-kondo
+                                                  ~(if-not (= 0 arity)
+                                                     (first argvec)
+                                                     :ok)))))))
+                     `(def ~sym-name :defined)))))))))
