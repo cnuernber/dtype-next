8f6ee3f1429751d4c324d35bedf632bec82b2f46
Chris Nuernberger <chris@techascent.com>
2022-01-15 07:26:33 -0700

    Resetting to next snapshot version.

--- java/tech/v3/datatype/ClojureHelper.java
+++ java/tech/v3/datatype/ClojureHelper.java
@@ -1,33 +1,11 @@
 package tech.v3.datatype;
 
-import clojure.lang.Symbol;
-import clojure.lang.Compiler;
-import clojure.lang.Namespace;
+import clojure.java.api.Clojure;
 import clojure.lang.IFn;
-import clojure.lang.Var;
-import clojure.lang.RT;
 
 public class ClojureHelper
 {
-  static boolean initialized = false;
-  public static boolean initialize() {
-    //unsynchronized guard.  RT.init is synchronized so
-    //this is faster but still safe.
-    if (initialized == false) {
-      initialized = true;
-      RT.init();
-      return true;
-    }
-    return false;
-  }
   public static IFn findFn(String nsName, String varName) {
-    try {
-      //Without RT initialization .var can return nonsense.
-      initialize();
-      return RT.var(nsName, varName);
-    } catch (Throwable e) {
-      throw new RuntimeException("Failed to find \"" + nsName + "/" + varName + "\"",
-				 e);
-    }
+    return (IFn)Clojure.var(nsName, varName);
   }
 }
